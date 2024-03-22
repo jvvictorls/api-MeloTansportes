daeaf25f7485a88d4dac2b67d1fe58b948a13702
João Victor <jvvictorls@hotmail.com>
2024-03-22 09:15:53 -0300

    Feat: added data to seeders

--- src/database/seeders/20240321023011-Users.ts
+++ src/database/seeders/20240321023011-Users.ts
@@ -10,9 +10,61 @@ module.exports = {
       name: 'João Victor',
       email: 'joao@dev.com',
       password: 'xablau',
-      createdAt: new Date(),
-      updatedAt: new Date()
-      }
+      },
+      {
+      name: 'Maria Silva',
+      email: 'maria@dev.com',
+      password: 'senha123',
+      },
+      {
+      name: 'Pedro Oliveira',
+      email: 'pedro@dev.com',
+      password: '123456',
+      },
+      {
+      name: 'Ana Santos',
+      email: 'ana@dev.com',
+      password: 'senha1234',
+      },
+      {
+      name: 'Lucas Pereira',
+      email: 'lucas@dev.com',
+      password: 'abcdef',
+      },
+      {
+      name: 'Camila Lima',
+      email: 'camila@dev.com',
+      password: 'senha321',
+      },
+      {
+      name: 'Bruno Souza',
+      email: 'bruno@dev.com',
+      password: '987654',
+      },
+      
+      {
+      name: 'Amanda Costa',
+      email: 'amanda@dev.com',
+      password: 'senha567',
+      },
+      
+      {
+      name: 'Gustavo Santos',
+      email: 'gustavo@dev.com',
+      password: 'senha432',
+      },
+      
+      {
+      name: 'Juliana Almeida',
+      email: 'juliana@dev.com',
+      password: '123abc',
+      },
+      
+      {
+      name: 'Felipe Oliveira',
+      email: 'felipe@dev.com',
+      password: 'qwerty',
+      },
   ], {});
   },
 
