acd44255b311364c466fee46af6d195f3126fcb0
João Victor <jvvictorls@hotmail.com>
2024-09-04 10:02:43 -0300

    Feat: Interface for padronized Response from db

---
+++ src/Interfaces/extraRoutes/INewExtraRoute.ts
@@ -0,0 +1,13 @@
+//purpose: Interface for new extra routes
+
+interface INewExtraRoute {
+  userId: number;
+  date: Date
+  costCenter: string;
+  driver: string;
+  client: string;
+  collaborators: { id: number; name: string; }[];
+  user: { id: number; name: string; };
+}
+
+export default INewExtraRoute;  
